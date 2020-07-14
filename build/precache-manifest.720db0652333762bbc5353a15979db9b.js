f2626028d48fb7f6cba9d9375f7f386298441f5c
Hanzila12 <hanzila.m12@gmail.com>
2020-07-14 13:50:22 +0500

    Add files via upload

---
+++ build/precache-manifest.720db0652333762bbc5353a15979db9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9298ccc95f633c779f4c29171526d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e45108dfa0336d84ce",
+    "url": "/static/css/main.1ed00699.chunk.css"
+  },
+  {
+    "revision": "4fae8f84f78c3914bef4",
+    "url": "/static/js/2.8ce056a6.chunk.js"
+  },
+  {
+    "revision": "907d435c4bf3db3f5ff6346b63fca1ef",
+    "url": "/static/js/2.8ce056a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e45108dfa0336d84ce",
+    "url": "/static/js/main.5c09b319.chunk.js"
+  },
+  {
+    "revision": "0fcf542389da4c7def2c",
+    "url": "/static/js/runtime-main.392db4dd.js"
+  }
+]);
